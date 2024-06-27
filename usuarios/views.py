763ddec8c348e1e72c9a09cfd0057ce8cfc42ee2
Nycollas Henich <nhhenich@gmail.com>
2024-06-27 13:11:56 -0300

    Autenticação simples feita

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,7 +1,43 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
+from django.contrib.auth import login as login_django
+from django.contrib.auth.decorators import login_required
 
 def cadastro(request):
-    return render(request, 'cadastro.html')
+    if request.method == "GET":
+        return render(request, 'cadastro.html')
+    else:
+         username = request.POST.get('username')
+         email = request.POST.get('email')
+         senha = request.POST.get('senha')
+
+         user = User.objects.filter(username=username).first()
+         
+         if user:
+             return HttpResponse('Usuário ja existente')
+
+         user = User.objects.create_user(username=username, email=email, password=senha) 
+         user.save()
+
+
+         return HttpResponse('Usuário cadastrado com sucesso')
 
 def login(request):
-    return render(request, 'login.html')
+    if request.method == 'GET':
+        return render(request, 'login.html')
+    else:
+        username = request.POST.get('username')
+        senha = request.POST.get('senha')
+
+        user = authenticate(username=username, password=senha)
+
+        if user:
+            login_django(request, user)
+            return HttpResponse('Autenticado')
+        else:
+            return HttpResponse('Email ou senha inválidos.')
+@login_required(login_url='/auth/login/')        
+def plataforma(request):
+    return HttpResponse('Plataforma aaa')
